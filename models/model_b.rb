5cb6e47a1db5b78bed08368b72334cb1916d4733
Kevin Mugele <kevin.mugele@gmail.com>
2021-05-21 11:22:26 -0600

    Add new beach house hash to model b

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+beach_house = {
+  location: "Cancun",
+  color: "Blue",
+  number_of_bedrooms: 5,
+  number_of_baths: 3.5,
+  beach_access: true,
+  has_pool: false,
+  items_provided_for_guests: ["towels", "beach tags", "toiletries"],
+  items_guests_must_bring: ["clothes", "soap", "sheets"]
+}
