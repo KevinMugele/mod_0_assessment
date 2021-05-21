455fae80977159a3dd0621b8a323c467a03c81a0
Kevin Mugele <kevin.mugele@gmail.com>
2021-05-21 11:17:17 -0600

    Add new macbook hash to model a

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+new_macbook = {
+  color: "Space grey",
+  processor: "M1 Chip",
+  storage_size_in_gb: 256,
+  memory_in_gb: 8,
+  is_fast: true,
+  is_new: true,
+  programs_to_install: ["Atom", "Slack", "Zoom"],
+  languages_to_learn: ["Ruby", "Javascript", "CSS"]
+}
