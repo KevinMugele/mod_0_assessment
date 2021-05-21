7d4807884ae65494907fd29256c8487d0bb91ee2
Kevin Mugele <kevin.mugele@gmail.com>
2021-05-21 11:05:33 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+animal = "cat"
+is_cuddly = true
+animal = "dog"
+num_8 = 37
+num_3 = 11
+quotient = num_3 / num_8
+favorite_films = ["Harry Potter", "Scott Pilgrim vs the World", "Hot Rod"]
+favorite_films.delete_at(0)
+puts animal.length
